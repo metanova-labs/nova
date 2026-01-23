ea35aac5fac336cb1ea86dd85d2f08706dd49236
cosmic-amanita <amanda@metanova-labs.com>
2026-01-23 22:44:01 +0000

    centalize configs

--- config/config_loader.py
+++ config/config_loader.py
@@ -12,65 +12,73 @@ def load_config(path: str = "config/config.yaml"):
         config = yaml.safe_load(f)
 
     # Load configuration options
-    weekly_target = config["protein_selection"]["weekly_target"]
-    num_antitargets = config["protein_selection"]["num_antitargets"]
+    small_molecule_target = config["protein_selection"]["small_molecule_target"].split(",")
+    nanobody_target = config["protein_selection"]["nanobody_target"].split(",")
 
     no_submission_blocks = config["competition"]["no_submission_blocks"]
-    boltz_weight = config["competition"]["boltz_weight"]
+    nanobody_weight = config["competition"]["nanobody_weight"]
     
-    validation_config = config["molecule_validation"]
-    antitarget_weight = validation_config["antitarget_weight"]
-    min_heavy_atoms = validation_config["min_heavy_atoms"]
-    min_rotatable_bonds = validation_config["min_rotatable_bonds"]
-    max_rotatable_bonds = validation_config["max_rotatable_bonds"]
-    num_molecules = validation_config["num_molecules"]
-    psichic_mode = validation_config["psichic_mode"]
-    entropy_bonus_threshold = validation_config["entropy_bonus_threshold"]
-    entropy_start_weight = validation_config["entropy_start_weight"]
-    entropy_start_epoch = validation_config["entropy_start_epoch"]
-    entropy_step_size = validation_config["entropy_step_size"]
-    molecule_repetition_weight = validation_config["molecule_repetition_weight"]
-    molecule_repetition_threshold = validation_config["molecule_repetition_threshold"]
-    num_molecules_boltz = validation_config["num_molecules_boltz"]
-    boltz_metric = validation_config["boltz_metric"]
-    combination_strategy = validation_config["combination_strategy"]
-    boltz_mode = validation_config["boltz_mode"]
-    sample_selection = validation_config["sample_selection"]
+    molecule_config = config["molecule_requirements"]
+    min_heavy_atoms = molecule_config["min_heavy_atoms"]
+    min_rotatable_bonds = molecule_config["min_rotatable_bonds"]
+    max_rotatable_bonds = molecule_config["max_rotatable_bonds"]
+    num_molecules = molecule_config["num_molecules"]
+    min_entropy = molecule_config["min_entropy"]
 
-    # Load protein constraints
-    protein_constraints = config["protein_constraints"]
-    binding_pocket = protein_constraints["binding_pocket"]
-    max_distance = protein_constraints["max_distance"]
-    force = protein_constraints["force"]
+    boltz_config = config["boltz2_config"]
+    boltz_metric = boltz_config["boltz_metric"]
+    combination_strategy = boltz_config["combination_strategy"]
+    boltz_mode = boltz_config["boltz_mode"]
+
+    boltzgen_config = config["boltzgen_config"]
+    boltzgen_rank_mode = boltzgen_config["boltzgen_rank_mode"]
+    boltzgen_rank_by = boltzgen_config["boltzgen_rank_by"]
+
+
+    nanobody_config = config["nanobody_requirements"]
+    num_sequences = nanobody_config["num_sequences"]
+    min_sequence_length = nanobody_config["min_sequence_length"]
+    max_sequence_length = nanobody_config["max_sequence_length"]
+    min_cysteines = nanobody_config["min_cysteines"]
+    cys_pair_min_separation = nanobody_config["cys_pair_min_separation"]
+    cys_pair_max_separation = nanobody_config["cys_pair_max_separation"]
+    max_homopolymer_run = nanobody_config["max_homopolymer_run"]
+    max_di_repeat_pairs = nanobody_config["max_di_repeat_pairs"]
+    reject_signal_peptides = nanobody_config["reject_signal_peptides"]
+    sp_window = nanobody_config["sp_window"]
+    sp_hydro_min_in_window = nanobody_config["sp_hydro_min_in_window"]
+    sp_scan_prefix = nanobody_config["sp_scan_prefix"]
 
     # Load reaction filtering configuration
     reaction_config = config["reaction_filtering"]
     random_valid_reaction = reaction_config["random_valid_reaction"]
 
     return {
-        'weekly_target': weekly_target,
-        'num_antitargets': num_antitargets,
+        'small_molecule_target': small_molecule_target,
+        'nanobody_target': nanobody_target,
         'no_submission_blocks': no_submission_blocks,
-        'antitarget_weight': antitarget_weight,
+        'nanobody_weight': nanobody_weight,
         'min_heavy_atoms': min_heavy_atoms,
         'min_rotatable_bonds': min_rotatable_bonds,
         'max_rotatable_bonds': max_rotatable_bonds,
         'num_molecules': num_molecules,
-        'psichic_mode': psichic_mode,
-        'entropy_bonus_threshold': entropy_bonus_threshold,
-        'entropy_start_weight': entropy_start_weight,
-        'entropy_start_epoch': entropy_start_epoch,
-        'entropy_step_size': entropy_step_size,
-        'molecule_repetition_weight': molecule_repetition_weight,
-        'molecule_repetition_threshold': molecule_repetition_threshold,
+        'min_entropy': min_entropy,
         'random_valid_reaction': random_valid_reaction,
-        'num_molecules_boltz': num_molecules_boltz,
         'boltz_metric': boltz_metric,
         'combination_strategy': combination_strategy,
         'boltz_mode': boltz_mode,
-        'binding_pocket': binding_pocket,
-        'max_distance': max_distance,
-        'force': force,
-        'boltz_weight': boltz_weight,
-        'sample_selection': sample_selection
+        'num_sequences': num_sequences,
+        'min_sequence_length': min_sequence_length,
+        'max_sequence_length': max_sequence_length,
+        'min_cysteines': min_cysteines,
+        'cys_pair_min_separation': cys_pair_min_separation,
+        'cys_pair_max_separation': cys_pair_max_separation,
+        'max_homopolymer_run': max_homopolymer_run,
+        'max_di_repeat_pairs': max_di_repeat_pairs,
+        'reject_signal_peptides': reject_signal_peptides,
+        'sp_window': sp_window,
+        'sp_hydro_min_in_window': sp_hydro_min_in_window,
+        'sp_scan_prefix': sp_scan_prefix,
+        'boltzgen_rank_mode': boltzgen_rank_mode,
+        'boltzgen_rank_by': boltzgen_rank_by,
     }
